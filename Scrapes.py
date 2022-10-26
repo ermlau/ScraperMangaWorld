dd75ffd71a41e2d9aa6e234bbf1d1f3285676cc2
ermlau <40149955+ermlau@users.noreply.github.com>
2022-10-26 14:44:34 +0200

    Update Scrapes.py

--- Scrapes.py
+++ Scrapes.py
@@ -3,9 +3,9 @@ import ScrapLib
 
 parser = argparse.ArgumentParser(description='Scraper for www.mangaworld.so')
 parser.add_argument('url', metavar='url', help='first page url')
-parser.add_argument('--pathCBR', metavar='pathCBR', default='cbrfile\\',
+parser.add_argument('--pc', metavar='pathCBR', default='cbrfile\\',
                     help='Folder where app save the cbr files')
-parser.add_argument('--pathTmpfile', metavar='pathTmpfile', default='tmpfile\\',
+parser.add_argument('--ptf', metavar='pathTmpfile', default='tmpfile\\',
                     help='Folder where app save temporary the image files')
 
 args = parser.parse_args()
